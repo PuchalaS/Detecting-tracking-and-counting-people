db9b1414aa8f478b1eb9ab15c2fb66ba07b2fe59
Sebastian Puchala <sebastian.puchala.95@gmail.com>
2021-06-04 15:54:26 +0200

    Removed tiny and yolov3 flags

--- save_model.py
+++ save_model.py
@@ -12,36 +12,28 @@ flags.DEFINE_float('score_thres', 0.2, 'define score threshold')
 
 
 def save_tf():
-  STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+  STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config()
 
   input_layer = tf.keras.layers.Input([FLAGS.input_size, FLAGS.input_size, 3])
-  feature_maps = YOLO(input_layer, NUM_CLASS, FLAGS.model, FLAGS.tiny)
+  feature_maps = YOLO(input_layer, NUM_CLASS)
   bbox_tensors = []
   prob_tensors = []
-  if FLAGS.tiny:
-    for i, fm in enumerate(feature_maps):
-      if i == 0:
-        output_tensors = decode(fm, FLAGS.input_size // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-      else:
-        output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-      bbox_tensors.append(output_tensors[0])
-      prob_tensors.append(output_tensors[1])
-  else:
-    for i, fm in enumerate(feature_maps):
-      if i == 0:
-        output_tensors = decode(fm, FLAGS.input_size // 8, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-      elif i == 1:
-        output_tensors = decode(fm, FLAGS.input_size // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-      else:
-        output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
-      bbox_tensors.append(output_tensors[0])
-      prob_tensors.append(output_tensors[1])
+  for i, fm in enumerate(feature_maps):
+    if i == 0:
+      output_tensors = decode(fm, FLAGS.input_size // 8, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
+    elif i == 1:
+      output_tensors = decode(fm, FLAGS.input_size // 16, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
+    else:
+      output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE)
+    bbox_tensors.append(output_tensors[0])
+    prob_tensors.append(output_tensors[1])
+
   pred_bbox = tf.concat(bbox_tensors, axis=1)
   pred_prob = tf.concat(prob_tensors, axis=1)
   boxes, pred_conf = filter_boxes(pred_bbox, pred_prob, score_threshold=FLAGS.score_thres, input_shape=tf.constant([FLAGS.input_size, FLAGS.input_size]))
   pred = tf.concat([boxes, pred_conf], axis=-1)
   model = tf.keras.Model(input_layer, pred)
-  utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
+  utils.load_weights(model, FLAGS.weights)
   model.summary()
   model.save(FLAGS.output)
 
