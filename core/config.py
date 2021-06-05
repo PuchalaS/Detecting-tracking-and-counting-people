bda03309ab05787605d0763e9ee95ba882305b6e
Sebastian Puchala <sebastian.puchala.95@gmail.com>
2021-06-05 21:31:38 +0200

    changed train and test paths

--- core/config.py
+++ core/config.py
@@ -22,11 +22,11 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 # Train options
 __C.TRAIN                     = edict()
 
-__C.TRAIN.ANNOT_PATH          = "./data/dataset/val2017.txt"
+__C.TRAIN.ANNOT_PATH          = "./data/dataset/annotations/train/train.txt"
 __C.TRAIN.BATCH_SIZE          = 2
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = 416
-__C.TRAIN.DATA_AUG            = True
+__C.TRAIN.DATA_AUG            = False
 __C.TRAIN.LR_INIT             = 1e-3
 __C.TRAIN.LR_END              = 1e-6
 __C.TRAIN.WARMUP_EPOCHS       = 2
@@ -38,7 +38,7 @@ __C.TRAIN.SECOND_STAGE_EPOCHS   = 30
 # TEST options
 __C.TEST                      = edict()
 
-__C.TEST.ANNOT_PATH           = "./data/dataset/val2017.txt"
+__C.TEST.ANNOT_PATH           = "./data/dataset/annotations/test/test.txt"
 __C.TEST.BATCH_SIZE           = 2
 __C.TEST.INPUT_SIZE           = 416
 __C.TEST.DATA_AUG             = False
